6f24189ba94d1edbad4c7a2828c3833b1dd80da0
Eugen Konkov <kes-kes@yandex.ru>
2018-12-10 14:54:49 +0200

    Initial command

---
+++ lib/MyApp/Command/db_env.pm
@@ -0,0 +1,30 @@
+package MyApp::Command::db_env;
+use Mojo::Base 'Mojolicious::Command';
+
+
+has description => 'Shell commands to setup environment for database';
+has usage => sub { shift->extract_usage };
+
+# create user App_cleaner with login createdb noinherit password 'password';
+
+
+sub run {
+	my( $self, @args ) =  @_;
+
+	my $DB  =  $self->app->config->{ db };
+	$DB->{ DSN } =  sprintf "dbi:%s:dbname=%s;host=%s;port=%s",  @$DB{ qw/ DRVR NAME HOST PORT / };
+	print "DB_$_=$DB->{ $_ }\n"   for sort keys %$DB;
+	1;
+}
+
+1;
+
+
+=head1 SYNOPSIS
+
+  Usage: APPLICATION db_env [OPTIONS]
+
+  Options:
+    NONE
+
+=cut
